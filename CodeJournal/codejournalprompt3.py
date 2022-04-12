a16f2d2eb81af2948b5bbeafc4044f1c96319377
mduvv <mduvvuri@ucsc.edu>
2022-04-12 13:54:15 -0700

    finished prompt3

--- CodeJournal/codejournalprompt3.py
+++ CodeJournal/codejournalprompt3.py
@@ -1,1 +1,35 @@
-sdfsdfsdfds
+'''
+Write a Python program that defines a function f(x) that
+returns x**3 + 8. In the main function of the program, 
+call f(x) with x = 9 and print the result.
+Use an if statement that executes if the result is larger than 27
+and prints “YAY!”.
+
+'''
+
+import numpy as mp		# import numpy librabry
+
+# function f(x) that returns x**3 + 8
+def f(x):
+	return x**3 + 8	
+
+
+# This defines our main()
+# function for our program
+def main():
+
+	x = 9
+	
+	print(f(x))
+
+	if f(x) > 27:
+		print("YAY!")
+
+
+
+# When we run the program
+# this executes first
+if __name__ == '__main__':
+	main()
+
+
