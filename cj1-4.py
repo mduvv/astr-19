4513dc476b29e43382dd1425e191497de6ee28c7
mduvv <mduvvuri@ucsc.edu>
2022-04-21 15:33:01 -0700

    Session 4 prompt for Code Journal 1

--- cj1-4.py
+++ cj1-4.py
@@ -4,6 +4,7 @@ Have the data members of the class represent the following physical parameters o
 the animal: length of the arms (float), length of the legs (float), number of eyes (int), does it have a tail? (bool), is it furry? (bool).
 Write an initialization function that sets the values of the data members when an instance of the class is created.
 Write a member function of the class to print out and describe the data members representing the physical characteristics of the animal.
+
 '''
 
 
